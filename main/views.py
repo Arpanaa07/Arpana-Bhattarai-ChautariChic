1a5eb24ec067ccf7d2d34c8a7e22940748ccfe45
arpana9 <arpanabhattarai007@gmail.com>
2025-12-14 10:21:07 +0545

    updates

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Hello, this is the main app!")
+    
+@login_required(login_url='login')
+def home(request):
+    return render(request, 'home.html')
 # Create your views here.
